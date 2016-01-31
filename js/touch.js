59a8159a7140270d85689a974e320408b3e6da72
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-01-31 15:37:50 +0900

    map

--- js/touch.js
+++ js/touch.js
@@ -1,5 +1,19 @@
 //DOMロード
 $(function(){
+//緯度経度
+var lon;
+var lat;
+
+//GPS
+navigator.geolocation.getCurrentPosition(function(evt){
+    //緯度
+    var lon =evt.cords.longitude;
+    //経度
+    var lat = evt.coords.latitude;
+},
+function(error){
+    console.log('GPS取れなかったよ');
+});
 
     //タッチイベント
     $('#ball').on('touchstart',function(){
@@ -13,4 +27,9 @@ $(function(){
     $('#ball').on('touchend',function(){
         $('#touchResult li').eq(2).text('touchendしたよ');
     });
+
+    $('#geo li').eq(0).text('緯度:' + lon);
+    $('#geo li').eq(1).text('経度:' + lat);
+
+
 });
