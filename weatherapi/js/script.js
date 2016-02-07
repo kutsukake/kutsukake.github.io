dc8d659b25604909fb84a644f703c44a51f5c185
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-02-07 11:42:08 +0900

    weather map api

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -6,5 +6,22 @@ $(function(){
         var lon = pos.coords.longitude;
 
         console.log(lat,lon);
+
+        //定数
+        const apikey ='0cf743caf2ac4e81cf61bb1c7314f8ac';
+
+        //openweather mapのお天気情報のURL
+        var url ='api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ lon +'&appid=' +apikey;
+
+        $.ajax({
+            url:url,
+            dataType:'json'
+        }).done(function(data){
+            console.log(data);
+
+        }).fail(function(){
+            console.log('ajax.error!!');
+        });
+
     });
 });
