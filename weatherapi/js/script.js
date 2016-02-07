07362ad73f9c4fafb2d1f8623c7ad09fd5975e7b
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-02-07 11:20:24 +0900

    weather map api

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -1,5 +1,6 @@
 $(function(){
     //GPS
+    // console.log('読み込み');
     navigator.geolocation.getCurrentPosition(function(pos){
         var lat = pos.coords.latitude;
         var lon = pos.coords.longitude;
