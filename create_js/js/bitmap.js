40e64be97589ea3a76a5144b84b22b33e713bff4
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-02-07 15:23:46 +0900

    createjs

--- create_js/js/bitmap.js
+++ create_js/js/bitmap.js
@@ -24,7 +24,7 @@ loadQueue.loadManifest(manifest);//読み込み開始
     });
 
 function showVege(){
-    var tomoto = new createjs.Bitmap(manifest[0].src);
+    var tomato = new createjs.Bitmap(manifest[0].src);
     tomato.x = 100;
     tomato.y = 100;
     stage.addChild(tomato);
