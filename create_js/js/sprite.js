f7226ee31d71574d55f763d1579d3785c56ff481
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-02-07 16:07:35 +0900

    spritejs

--- create_js/js/sprite.js
+++ create_js/js/sprite.js
@@ -1,8 +1,8 @@
 //先読みするリスト
 var manifest = [
-    {src:'img/tomato.png'},
-    {src:'img/pumpkin.png'},
-    {src:'img/hourensou.png'}
+    {src:'img/calcium.png'},
+    {src:'img/iron.png'},
+    {src:'img/vitaminC.png'}
 
 ];
 
@@ -28,19 +28,23 @@ loadQueue.addEventListener('progress',function(evt){
 });
 
 
+
+
 function showVege(){
-    var tomato = new createjs.Bitmap(manifest[0].src);
-    tomato.x = 100;
-    tomato.y = 100;
-    stage.addChild(tomato);
+    //スプライトシートの設定
+    var vcSprite = new createjs.SpriteSheet(vitaminCSprite);
+
+    //スプライトの設定
+    var vitaminC = new create.Sprite(vcSprite,'stand');
+    stage.addChaild(vitaminC);
+
+    vitaminC.x = 150;
+    vitaminC.y = 480;
+    vitaminC.alpha = 0;
+    //アニメーション
+    createjs.Tween.get(vitaminC).to({alpha:1},500).wait(1000).to({500},4000);
 
 
-    var pumpkin = new createjs.Bitmap(manifest[1].src);
-    pumpkin.x = 300;
-    pumpkin.y = 300;
-    stage.addChild(pumpkin);
-
-    stage.update();
 }
 
 
