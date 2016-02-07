5de773c6382e511c02d0a1705092961ed547da52
kutsukake <MAYUMI@kutsukakeshinyumi-no-MacBook-Pro.local>
2016-02-07 16:41:55 +0900

    spritejs

--- create_js/js/vegeSprite.js
+++ create_js/js/vegeSprite.js
@@ -7,3 +7,12 @@ var vitaminCSprite = {
         walk:[0,33]
     }
 }
+
+var ironSprite = {
+    images:['img/iron.png'],
+    frames:{width:160,height:160},
+    animations:{
+        stand:34,
+        walk:[0,33]
+    }
+}
